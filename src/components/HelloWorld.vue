77f9d0bb5241d795e35c69de446317b27946edb5
taktak7890 <taku-1218@hotmail.co.jp>
2024-02-07 22:41:40 +0900

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import Tile from './Tile.vue'
+
+defineProps({
+    msg: String,
+})
+
+const count = ref({ a: 1, b: 2 })
+
+const tile: boolean[][] = [];
+for (let i = 0; i < 10; i++) {
+    tile.push([])
+    for (let j = 0; j < 10; j++) {
+        tile[i].push(false);
+    }
+}
+</script>
+
+<template>
+    <h1>{{ msg }}</h1>
+
+    <div class="card">
+        <button type="button" @click="count.a++">count is {{ { count } }}</button>
+        <p>
+            Edit
+            <code>components/HelloWorld.vue</code> to test HMR
+        </p>
+        <div id="app">
+            <table style="border-collapse: collapse;">
+                <tr v-for="(i, index_i) in tile">
+                    <td v-for="(j, index_j) in i">
+                        <Tile :tile_key="`${index_j}_${index_i}`" v-if="'a'"></Tile>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </div>
+
+    <p>
+        Check out
+        <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the official Vue + Vite
+        starter
+    </p>
+    <p>
+        Install
+        <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
+        in your IDE for a better DX
+    </p>
+    <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+</template>
+
+<style scoped>
+.read-the-docs {
+    color: #888;
+}
+</style>
---
+++ src/components/Tile.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+
+defineProps({
+    tile_key: String,
+})
+const selected = ref(true)
+
+function onClick(args: MouseEvent) {
+    selected.value = !selected.value;
+    console.log(selected.value)
+}
+</script>
+
+<template>
+    {{ selected }}
+    <div :id="`tile_${tile_key}`" v-bind:class="selected ? 'tile' : 'tile-selected'" @click="onClick">
+        <div :style="{ width: '100%', height: '100%' }">10</div>
+    </div>
+</template>
+
+<style>
+.tile {
+    background-color: aqua;
+    border: 1px black solid;
+    width: 2rem;
+    height: 2rem;
+    margin: 0%;
+    padding: 0%;
+}
+
+.tile-selected {
+    background-color: rgb(255, 0, 191);
+    border: 1px black solid;
+    width: 2rem;
+    height: 2rem;
+    margin: 0%;
+    padding: 0%;
+}
+
+.tile:hover {
+    background-color: aquamarine;
+}
+</style>
